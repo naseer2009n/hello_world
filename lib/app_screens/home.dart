c8fafecb8ff30d6ef9e16fbebfacf35f48499182
Mohammed Naseer n <naseer2009n@gmail.com>
2019-09-16 21:08:36 +0530

    Using image in flutter

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -6,35 +6,82 @@ class Home extends StatelessWidget{
     return Container(
       alignment: Alignment.center,
       color: Colors.deepPurple,
-      child: Row(
+      padding: EdgeInsets.only(left:10.0, top: 40.0),
+      child: Column(
         children: <Widget>[
-          Expanded(child: Text(
-            "Spice Jet",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-                decoration: TextDecoration.none,
-                fontSize: 35.0,
-                fontFamily: 'Raleway',
-                fontWeight: FontWeight.w700,
-                color: Colors.white
+          Row(
+            children: <Widget>[
+              Expanded(child: Text(
+                "Spice Jet",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 35.0,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white
 
-            ),
-          )),
-          Expanded(child: Text(
-            "From Mumbai to Bangalore via New Delhi",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-                decoration: TextDecoration.none,
-                fontSize: 20.0,
-                fontFamily: 'Raleway',
-                fontWeight: FontWeight.w700,
-                color: Colors.white
+                ),
+              )),
+              Expanded(child: Text(
+                "From Mumbai to Bangalore via New Delhi",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 20.0,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white
 
-            ),
-          )),
+                ),
+              )),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(child: Text(
+                "Spice Jet",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 35.0,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white
+
+                ),
+              )),
+              Expanded(child: Text(
+                "From Mumbai to Bangalore via New Delhi",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 20.0,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white
+
+                ),
+              )),
+            ],
+          ),
+          FlightImageAsset()
         ],
       )
+
+
+
     );
+  }
+
+}
+
+class FlightImageAsset extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage('images/flight.png');
+    Image image = Image(image: assetImage);
+    return Container(child: image,);
   }
 
 }
